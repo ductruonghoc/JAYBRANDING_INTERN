4f4c430fdb87afab0072d1d32df76cd045091943
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-09-12 18:44:25 +0700

    v1.0.8

--- jaybranding_intern/src/app/page.tsx
+++ jaybranding_intern/src/app/page.tsx
@@ -4,16 +4,6 @@ import Header from "./header";
 import ThumbNail from "./thumbnail";
 import Footer from "./footer";
 import Popup from "./popup";
-import type { Viewport } from 'next'
- 
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  // Also supported by less commonly used
-  // interactiveWidget: 'resizes-visual',
-}
 export default function Gallery() {
   return(
     <div className="relative w-full">
